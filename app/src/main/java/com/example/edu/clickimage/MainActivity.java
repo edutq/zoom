851b8851ca8df192e4eb801813dfaa12b54f0f77
Edu <etavarezq@gmail.com>
2018-06-04 10:42:37 -0500

    selection working

--- app/src/main/java/com/example/edu/clickimage/MainActivity.java
+++ app/src/main/java/com/example/edu/clickimage/MainActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.github.chrisbanes.photoview.OnPhotoTapListener;
@@ -16,45 +17,66 @@ import com.github.chrisbanes.photoview.PhotoView;
 import com.github.chrisbanes.photoview.PhotoViewAttacher;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener
 {
+    PhotoViewAttacher photoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ZoomView zoom = (ZoomView)findViewById(R.id.zoomView);
+
+        RelativeLayout lo = (RelativeLayout) findViewById(R.id.layo);
         ImageView iv =  findViewById (R.id.image);
-        final ImageView iv2 = findViewById(R.id.image_areas);
 
-        PhotoViewAttacher photoView = new PhotoViewAttacher(iv);
-        photoView.update();
-
-        if (iv != null) {
-            photoView.setOnViewTapListener(new OnViewTapListener() {
-                @Override
-                public void onViewTap(View view, float x, float y) {
-                    int touchColor = getHotspotColor (R.id.image_areas, (int) x, (int) y, iv2);
-                    Log.i ("RGB calculado", String.valueOf(Color.red(touchColor)) +" "+String.valueOf(Color.green(touchColor)) + " " + String.valueOf(Color.blue(touchColor)));
-                    // Compare the touchColor to the expected values. Switch to a different image, depending on what color was touched.
-                    // Note that we use a Color Tool object to test whether the observed color is close enough to the real color to
-                    // count as a match. We do this because colors on the screen do not match the map exactly because of scaling and
-                    // varying pixel density.
-                    ColorTool ct = new ColorTool ();
-                    int tolerance = 20;
-                    //Log.i ("color encontrado", String.valueOf(touchColor));
-                    //Log.i ("color calculado", String.valueOf(Color.parseColor("#FFFFFF")));
-                    if (ct.closeMatch (Color.BLACK, touchColor, tolerance)) toast("le diste al negro");
-                    else if (ct.closeMatch (Color.parseColor("#ED1C24"), touchColor, tolerance)) toast("le diste al rojo");
-                    else if (ct.closeMatch (Color.parseColor("#FFF200"), touchColor, tolerance)) toast("le diste al amarillo");
-
-                    Log.i ("posicion", String.valueOf(x) + " " + String.valueOf(y));
-                }
-            });
+        if(iv != null) {
+            iv.setOnTouchListener(this);
         }
+
 
     }
 
+    public boolean onTouch (View v, MotionEvent ev)
+    {
+        ImageView iv2 = findViewById(R.id.image_areas);
 
+        final int evX = (int) ev.getX();
+        final int evY = (int) ev.getY();
+
+        int touchColor = getHotspotColor (R.id.image_areas, evX, evY, iv2);
+        Log.i ("RGB calculado", String.valueOf(Color.red(touchColor)) +" "+String.valueOf(Color.green(touchColor)) + " " + String.valueOf(Color.blue(touchColor)));
+        // Compare the touchColor to the expected values. Switch to a different image, depending on what color was touched.
+        // Note that we use a Color Tool object to test whether the observed color is close enough to the real color to
+        // count as a match. We do this because colors on the screen do not match the map exactly because of scaling and
+        // varying pixel density.
+        ColorTool ct = new ColorTool ();
+        int tolerance = 20;
+
+        if (ct.closeMatch (Color.BLACK, touchColor, tolerance)) {
+            //toast("le diste al negro");
+            ImageView viewnegro = (ImageView) findViewById(R.id.fotonegro);
+
+            viewnegro.setVisibility(View.VISIBLE);
+        }
+        else if (ct.closeMatch (Color.parseColor("#ED1C24"), touchColor, tolerance)) {
+            ImageView viewrojo = (ImageView) findViewById(R.id.fotorojo);
+
+            viewrojo.setVisibility(View.VISIBLE);
+            //toast("le diste al rojo");
+        }
+        else if (ct.closeMatch (Color.parseColor("#FFF200"), touchColor, tolerance)) {
+            //toast("le diste al amarillo");
+            ImageView viewamarillo = (ImageView) findViewById(R.id.fotoamarillo);
+
+            viewamarillo.setVisibility(View.VISIBLE);
+        }
+
+        Log.i ("posicion", String.valueOf(evX) + " " + String.valueOf(evY));
+
+        return true;
+    }
     public int getHotspotColor (int hotspotId, int x, int y, ImageView iv) {
 
         if (iv == null) {
